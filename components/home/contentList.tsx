fc5deae3773a9638f22357fce9faa78de564d0ff
Taewoong-H <taeung2008@gmail.com>
2022-03-05 23:51:56 +0900

    fix: contentList css change

--- components/home/contentList.tsx
+++ components/home/contentList.tsx
@@ -1,24 +1,64 @@
+import Image from 'next/image';
+
 const ContentList = ({ content }: any) => {
   return (
     <>
       {content.map((item: any) => {
+        console.log(item);
+        const today = new Date();
+        const itemDate = new Date(item.raw_date);
+        const calcYear = today.getFullYear() - itemDate.getFullYear();
         return (
           <div className="card mb-3" key={item.id}>
             <div className="row g-0">
               <div className="col-md-4">
                 <img className="img-fluid rounded-start" src={item.ogImage} alt="..." />
               </div>
-              <div className="col-md-8">
+              <div className="col-md-8 card-container">
                 <div className="card-body">
                   <h5 className="card-title">{item.ogTitle}</h5>
                   <p className="card-text">{item.opinion}</p>
+                  <div className="card-user row">
+                    <Image
+                      src={item.current_user.profile_img}
+                      alt="profile-image"
+                      className="profile-image col"
+                      width={50}
+                      height={50}
+                    ></Image>
+                    <p className="col">{item.current_user.nickname}</p>
+                  </div>
                 </div>
+                <div className="calc-year">무려 {calcYear}년전 발행</div>
               </div>
             </div>
           </div>
         );
       })}
-      <style jsx>{``}</style>
+      <style jsx global>{`
+        .img-fluid {
+          width: 100%;
+          height: 15rem;
+        }
+        .card-container {
+          position: relative;
+        }
+        .card-title {
+          width: 80%;
+        }
+        .calc-year {
+          position: absolute;
+          top: 5%;
+          right: -1%;
+          background-color: #fbeaeb;
+          color: #2e3c7e;
+          padding: 10px;
+          border-radius: 5px;
+        }
+        .profile-image {
+          border-radius: 50px !important;
+        }
+      `}</style>
     </>
   );
 };
