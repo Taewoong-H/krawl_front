b2d257866c61a10d3c67d2200259e9bb7d401a04
Taewoong-H <taeung2008@gmail.com>
2022-03-18 15:19:16 +0900

    style: 파비콘 적용

--- components/home/competition.tsx
+++ components/home/competition.tsx
@@ -15,7 +15,13 @@ const Competition = ({ isContent }: any) => {
         <div></div>
       )}
       {isContent ? (
-        <p className="fs-5 text-muted">만 3세 미만 콘텐츠 출입금지</p>
+        <>
+          <h5>5년 이상 묵힌 콘텐츠만 취급</h5>
+          <p className="fs-6 text-muted">
+            우리는 따끈따끈한 정보를 좆지 않습니다. 곰팡내도 좀 나고 먼지도 쌓인,
+            <br /> 하지만 여전히 번뜩이는 통찰력을 가진 콘텐츠만을 취급합니다.
+          </p>
+        </>
       ) : (
         <p className="fs-5 text-muted">좋은 콘텐츠들은 휘발되어서 안 된다</p>
       )}
